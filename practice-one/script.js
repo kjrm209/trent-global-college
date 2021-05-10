bd375df4098b2d66452e5e9d5e97b2b585d5dd2a
kjrm209 <61448722+kjrm209@users.noreply.github.com>
2021-05-10 16:23:25 +0800

    10-05-2021 @ 1625 HRS

---
+++ practice-one/script.js
@@ -0,0 +1,11 @@
+function myOne()        {
+document.getElementById("demo").innerHTML="You are now on one.html !"
+}
+
+function myTwo()        {
+document.getElementById("demo").innerHTML="You are now on two.html !"
+}
+
+function myThree()        {
+document.getElementById("demo").innerHTML="You are now on three.html !"
+}   
